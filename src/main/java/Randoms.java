84aa58e4bb162de5dea2e99c7cad3a644408b40f
Anigko <anigko@yandex.ru>
2022-04-24 23:02:53 +0300

    Initial commit

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+
+public class Randoms implements Iterable<Integer> {
+    private int min;
+    private int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+
+        return new Iterator<>() {
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                int df = max - min;
+                return (int) (Math.random() * ++df) + min;
+            }
+        };
+    }
+}
+
+
+
+
+
